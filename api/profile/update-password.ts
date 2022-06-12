edaa8ec46edd26b54bda7aa79f1d4b8e8315e86d
Conan Malanaphy <conanmalanaphy@gmail.com>
2022-06-12 18:46:20 +0100

    profile page template

---
+++ api/profile/update-password.ts
@@ -0,0 +1,19 @@
+import { NextApiHandler } from 'next'
+import { supabase } from '../../libs/initSupabase'
+
+const user: NextApiHandler = async (req, res) => {
+    if (req.method === 'POST') {
+        // probs do some validation on this??
+        const { user, error } = await supabase.auth.update({email: req.body})
+
+        if (!error) {
+            res.status(200).json(user)
+        } else {
+            console.log(error)
+            res.status(404).end()
+        }
+        return
+    }
+}
+
+export default user
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,11 +1,28 @@
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
 import CssBaseline from '@mui/material/CssBaseline'
+import Grid from '@mui/material/Grid'
+import Link from '@mui/material/Link'
 import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
 import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import * as React from 'react'
 import Wrapper from '../components/Wrapper/Wrapper'
+import { supabase } from '../libs/initSupabase'
+
 
 function Profile() {
+    const profile = supabase.auth.user()
+
+    const handleSubmit = (
+        event?: React.SyntheticEvent | Event,
+        reason?: string
+    ) => {
+        console.log(profile)
+    }
+
     return (
         <Box sx={{ display: 'flex' }}>
             <CssBaseline />
@@ -34,7 +51,111 @@ function Profile() {
                             marginTop: 2,
                         }}
                     >
-                        Profile
+                        <Typography component="h1" variant="h5">
+                            Update Email
+                        </Typography>
+                        <Box
+                            component="form"
+                            noValidate
+                            onSubmit={handleSubmit}
+                            sx={{ mt: 3 }}
+                        >
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        value={profile?.email || ""}
+                                        id="email"
+                                        label="Current Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                        disabled
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="New Email Address"
+                                        id="password"
+                                        autoComplete="new-password"
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Confirm
+                            </Button>
+                        </Box>
+                    </Paper>
+                    <Paper
+                        sx={{
+                            p: 2,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            height: '100%',
+                            width: '100%',
+                            marginTop: 2,
+                        }}
+                    >
+                        <Typography component="h1" variant="h5">
+                            Update Password
+                        </Typography>
+                        <Box
+                            component="form"
+                            noValidate
+                            onSubmit={handleSubmit}
+                            sx={{ mt: 3 }}
+                        >
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Current Password"
+                                        name="email"
+                                        autoComplete="email"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="New Password"
+                                        type="password"
+                                        id="password1"
+                                        autoComplete="new-password"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Confirm New Password"
+                                        type="password"
+                                        id="password2"
+                                        autoComplete="new-password"
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Confirm
+                            </Button>
+                        </Box>
                     </Paper>
                 </Container>
             </Box>
