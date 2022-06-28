293665b73099fe5fc513c88857cc046f4cd933ef
Conan Malanaphy <conanmalanaphy@gmail.com>
2022-06-28 16:23:00 +0100

    missing import

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -8,6 +8,7 @@ import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableFooter from '@mui/material/TableFooter'
 import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
 
 export default function Pricing(): JSX.Element {
     return (
