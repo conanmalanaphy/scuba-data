b33e6a9ad9119869c9a50ab7981c4e3b1e256928
Conan Malanaphy <conanmalanaphy@gmail.com>
2022-07-25 17:00:31 +0100

    add helper functions

---
+++ libs/helperFunctions.ts
@@ -0,0 +1,11 @@
+interface ConditionalWrapperProps {
+    condition: boolean
+    wrapper: (children: JSX.Element) => JSX.Element
+    children: JSX.Element
+}
+
+export const ConditionalWrapper = ({
+    condition,
+    wrapper,
+    children,
+}: ConditionalWrapperProps) => (condition ? wrapper(children) : children)
